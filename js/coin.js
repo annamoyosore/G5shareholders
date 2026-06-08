99d6dadbc03bcfa66c8754da235e535ac292cada
annamoyosore <annamoyosore@gmail.com>
2026-06-08 08:18:43 +0100

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -38,7 +38,7 @@ const addresses={
 bnb:"0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",
 eth:"0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",
 btc:"bc1qsxzmwlwn2j9g2rm6352y80u38vz2a7p326m4rv",
-usdt trc:"TWpZiDpjejjyUaxDUwnSGbw1dH1JQvzaAy",
+usdt:"TWpZiDpjejjyUaxDUwnSGbw1dH1JQvzaAy",
 sol:"5a39EMz6Hm3k1gFcMmTxojPijfiDzNxQcWhDpRUtgDRv"
 
 };
